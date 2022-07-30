1496b88ce3f681ead1007a9437334ff6d0237740
Viacheslav <vaceslavkozin619@gmail.com>
2022-07-30 12:43:22 +0200

    Add doc strings

--- multi-eshell.el
+++ multi-eshell.el
@@ -1,10 +1,27 @@
-(setq eshell-last-id nil)
-(setq eshell-max-id 0) 
+;; multi-eshell.el - GNU Emacs extension for managing multiple eshell buffers. 
 
-(defun get-eshell-buf-name (num)
+;; Copyright (C) 2022 Viacheslav Chepelyk-Kozhin.
+;;
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later version.
+;; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+;; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+(defvar-local ees--eshell-last-id nil
+  "Last identifier for eshell buffer")
+
+(defvar ess--eshell-max-id 0
+  "Amount of eshell buffers created with ees") 
+
+(defun ees--get-eshell-buf-name (num)
+  "Get the name of the buffer created with ees/eshell-new where NUM is the buffer id."
   (concat "*eshell*<" (number-to-string num) ">"))
 
-(defun eshell-new-rec (cnt)
+(defun ees--eshell-new-rec (cnt)
   (if (get-buffer (get-eshell-buf-name cnt))
 	  (eshell-new-rec (+ cnt 1))
 	  (eshell
@@ -13,18 +30,21 @@
 		   (setq eshell-max-id cnt))
 		 (setq eshell-last-id cnt)))))
 
-(defun eshell-new (&optional cnt)
+(defun ees/eshell-new (&optional buf-id)
+  "Create new eshell buffer with the minimal available id. You can specify buffer id explicitly with an optional BUF-ID argument."x
   (interactive)
-  (eshell-new-rec (or cnt 1)))
+  (eshell-new-rec (or buf-id 1)))
 
-(defun eshell-last-rec (id &optional ebuffer)
+(defun ees--eshell-last-rec (id &optional ebuffer)
   (if (equal eshell-max-id id)
 	  (if ebuffer
 		  (switch-to-buffer ebuffer)
 		(eshell (setq eshell-last-id 1)))
 	(eshell-last-rec (+ id 1) (get-buffer (get-eshell-buf-name id)))))
 
-(defun eshell-last (&optional id)
+(defun ees/eshell-last (&optional id)
+  "Switch to last available eshell buffer. If no eshell buffers found - create one.
+Optionally accepts ID of the eshell buffer to switch directly."
   (interactive)
   (let*
 	  ((eshell-id (or id eshell-last-id))
@@ -33,3 +53,4 @@
 	  (switch-to-buffer eshell-buf)
 	  (eshell-last-rec 1))))
 
+(provide 'emacs-multi-eshell)
